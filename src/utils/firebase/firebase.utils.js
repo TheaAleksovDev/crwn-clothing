7456b726b9edb5f5c435f54a26335f4f70a686ae
Tea Aleksov <aleksov.tea@gmail.com>
2022-10-10 18:50:17 +0200

    new commit

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import {initializeApp} from 'firebase/app'
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider,} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRYB2b_3v1oDVlydTg39QAvZfDTQcri-I",
+  authDomain: "crwn-clothing-4d534.firebaseapp.com",
+  projectId: "crwn-clothing-4d534",
+  storageBucket: "crwn-clothing-4d534.appspot.com",
+  messagingSenderId: "313091561096",
+  appId: "1:313091561096:web:d490c96d47047863ea713d"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
