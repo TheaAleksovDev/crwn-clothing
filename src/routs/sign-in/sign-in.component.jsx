88f92cd4c97dc0a6c4ee8df301bef9825239d51e
Tea Aleksov <aleksov.tea@gmail.com>
2022-10-11 19:48:46 +0200

    new

--- src/routs/sign-in/sign-in.component.jsx
+++ src/routs/sign-in/sign-in.component.jsx
@@ -1,23 +1,28 @@
+import { useEffect  } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
   auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
   signInWithGoogleRedirect,
   } from '../../utils/firebase/firebase.utils';
-import { useEffect  } from 'react';
-import { getRedirectResult } from 'firebase/auth';
+
 
 const SignIn = () => {
 
-  useEffect(async()=>{
+  useEffect(async()=> {
     const response = await getRedirectResult(auth)
     console.log(response)
-  },[])
+  }, [])
 
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user)
   };
+
+
+
 
 
   return (
